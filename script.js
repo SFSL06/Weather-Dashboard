6d15468897948ab975251999b9bca12e97d8aa70
Ammar Ozair <sabeenjamil100@yahoo.com>
2023-08-16 10:28:24 +0100

    Added style improved search history

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // $(document).ready(function () {
   //URL to call the API to find current weather
-  var apiURL = `https://api.openweathermap.org/data/2.5/weather?`;
+  
+   var apiURL = `https://api.openweathermap.org/data/2.5/weather?`;
 
     
    var apiForecast = `https://api.openweathermap.org/data/2.5/forecast?`;
@@ -45,8 +46,14 @@
         dateToday = dayjs();
         dateToday = dayjs(dateToday).format('DD/MM/YYYY');
         // Transfer content to HTML
-        //var imgCurrent = $("#currentWeather").attr("src", "data.weather[0].icon")
-         $(".city").html("<h1>" + data.name + ' ' + dateToday + "</h1>");
+        //create an element to store weather icon and display it
+        var weatherImg = data.weather[0].icon;
+        // Retrieving the URL for the image
+        
+        
+         $(".city").html("<h1>" + data.name + ' ' + dateToday + "</h1>" + " ");
+         $("#currentWeather").attr("src", "https://openweathermap.org/img/wn/" + weatherImg + "@2x.png");
+         //$(".city").append(iconForecast);
          $(".temp").text("Temperature (C) " + temp.toFixed(2));
          $(".wind").text("Wind Speed: " + data.wind.speed);
          $(".humidity").text("Humidity: " + data.main.humidity);
@@ -68,29 +75,26 @@
         var tempForecast = "0";
         var windForecast = "0";
         var humidityForecast = "0";
+        
+//  var image = $("<img>").attr("src", imgURL);
 
+//  // Appending the image
+//  movieDiv.append(image);
         // currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
         // currentPicEl.setAttribute("alt", response.data.weather[0].description);
         //Get 5 day forecast
          $("#forecastHeader").text("Forecast");
           //forecastIndex  = (i * 8 ) + 1;
+         
           var forecastCardEl = $("#day1")
-          var dateForecast = new Date(data.list[1].dt_txt);
-          dateForecast = dayjs(dateForecast).format('DD/MM/YYYY');
-          var dateForecast = $("<h5>").text(dateForecast);
-          forecastCardEl.append(dateForecast);
-          var tempForecast = $("<p>").text("Temp (C) " + (data.list[1].main.temp - 273.15).toFixed(2));
-          forecastCardEl.append(tempForecast);
-          var windForecast = $("<p>").text("Wind: " + data.list[1].wind.speed);
-          forecastCardEl.append(windForecast);
-          var humidityForecast = $("<p>").text("Humidity: " + data.list[1].main.humidity);
-          forecastCardEl.append(humidityForecast);
-          forecastDaily.append(forecastCardEl);
-         var forecastCardEl = $("#day2")
           var dateForecast = new Date(data.list[9].dt_txt);
           dateForecast = dayjs(dateForecast).format('DD/MM/YYYY');
           var dateForecast = $("<h5>").text(dateForecast);
           forecastCardEl.append(dateForecast);
+          var weatherIcon = data.list[9].weather[0].icon;
+        // Retrieving the URL for the image
+          var iconForecast = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+          forecastCardEl.append(iconForecast)
           var tempForecast = $("<p>").text("Temp (C) " + (data.list[9].main.temp - 273.15).toFixed(2));
           forecastCardEl.append(tempForecast);
           var windForecast = $("<p>").text("Wind: " + data.list[9].wind.speed);
@@ -99,11 +103,15 @@
           forecastCardEl.append(humidityForecast);
           forecastDaily.append(forecastCardEl);
          
-          var forecastCardEl = $("#day3")
+          var forecastCardEl = $("#day2")
           var dateForecast = new Date(data.list[17].dt_txt);
           dateForecast = dayjs(dateForecast).format('DD/MM/YYYY');
           var dateForecast = $("<h5>").text(dateForecast);
           forecastCardEl.append(dateForecast);
+          var weatherIcon = data.list[17].weather[0].icon;
+        // Retrieving the URL for the image
+          var iconForecast = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+          forecastCardEl.append(iconForecast)
           var tempForecast = $("<p>").text("Temp (C) " + (data.list[17].main.temp - 273.15).toFixed(2));
           forecastCardEl.append(tempForecast);
           var windForecast = $("<p>").text("Wind: " + data.list[17].wind.speed);
@@ -112,11 +120,15 @@
           forecastCardEl.append(humidityForecast);
           forecastDaily.append(forecastCardEl);
 
-          var forecastCardEl = $("#day4")
+          var forecastCardEl = $("#day3")
           var dateForecast = new Date(data.list[25].dt_txt);
           dateForecast = dayjs(dateForecast).format('DD/MM/YYYY');
           var dateForecast = $("<h5>").text(dateForecast);
           forecastCardEl.append(dateForecast);
+          var weatherIcon = data.list[25].weather[0].icon;
+        // Retrieving the URL for the image
+          var iconForecast = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+          forecastCardEl.append(iconForecast)
           var tempForecast = $("<p>").text("Temp (C) " + (data.list[25].main.temp - 273.15).toFixed(2));
           forecastCardEl.append(tempForecast);
           var windForecast = $("<p>").text("Wind: " + data.list[25].wind.speed);
@@ -125,11 +137,15 @@
           forecastCardEl.append(humidityForecast);
           forecastDaily.append(forecastCardEl);
 
-          var forecastCardEl = $("#day5")
+          var forecastCardEl = $("#day4")
           var dateForecast = new Date(data.list[33].dt_txt);
           dateForecast = dayjs(dateForecast).format('DD/MM/YYYY');
           var dateForecast = $("<h5>").text(dateForecast);
           forecastCardEl.append(dateForecast);
+          var weatherIcon = data.list[33].weather[0].icon;
+        // Retrieving the URL for the image
+          var iconForecast = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+          forecastCardEl.append(iconForecast)
           var tempForecast = $("<p>").text("Temp (C) " + (data.list[33].main.temp - 273.15).toFixed(2));
           forecastCardEl.append(tempForecast);
           var windForecast = $("<p>").text("Wind: " + data.list[33].wind.speed);
@@ -150,11 +166,11 @@
             
             var historyItem = $("<input>")
             historyItem.text(searchHistory[i]);
-            //$("historyItem").addClass("form-control d-block bg-white");
             historyItem.attr("type", "text");
             historyItem.attr("readonly", true);
             historyItem.attr("value", searchHistory[i]);
             historyItem.addClass("search-btn") 
+            historyItem.attr("background-colour", "grey");
             $(historyEl).append(historyItem);
             
             historyItem.attr("readonly", true);
@@ -172,9 +188,6 @@
         getWeather(queryURL, queryForecast);
       });
     }
-
-   
-
 
     // Clear History button
     $("#clear-history").on("click", function () {
